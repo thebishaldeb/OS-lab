37842e7d748a24dc5a1dc2a17990841a7fd5cad4
Bishal Deb <bishaldeb656@gmail.com>
2019-08-29 15:21:31 +0530

    Assignment 2a done!

---
+++ Assignment 2/as1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <utmp.h>
+#include <errno.h>
+#include <signal.h>
+
+#define BUF_SIZE 100
+
+void userFunction()
+{
+    char buf[BUF_SIZE];
+    pid_t child;
+    printf("\nCreating Fork...\n\n");
+    child = fork();
+    if (child < 0)
+    {
+        sprintf(buf, "Fork error\n");
+        write(1, buf, strlen(buf));
+        exit(1);
+    }
+    if (child == 0)
+    {
+        sprintf(buf, "Under Child\n\tPID is %d and PPID is %d\n", getpid(), getppid());
+        write(1, buf, strlen(buf));
+    }
+    else
+    {
+        wait(NULL);
+        sprintf(buf, "Under Parent\n\tPID is %d and PPID is %d\n", getpid(), getppid());
+        write(1, buf, strlen(buf));
+    }
+    exit(0);
+}
+
+int main()
+{
+    void sigint_handler(int sig);
+    printf("\nPress ^C to send the signal!\n");
+    while (1)
+    {
+        if (signal(SIGINT, sigint_handler) == SIG_ERR)
+        {
+            perror("Signal");
+            exit(1);
+        }
+    }
+    exit(0);
+}
+
+void sigint_handler(int sig)
+{
+    printf("\nSignal received!\n");
+    userFunction();
+}
